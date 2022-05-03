8f62897ddd3848789b97018e4f03bbb3d38ec84e
mannbmx <mannbmx@gmail.com>
2022-05-02 23:43:28 -0400

    Added time calc function

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const video = document.querySelector('video'), frameRate = 1/60;
+const video = document.querySelector('video'), frameRate = 1/25;
 
 //frame forward and back buttons
-const frameBackd = document.querySelector('.frameBack');
-const frameForwards = document.querySelector('.frameForward');
 
 const frameBack = () => {
     video.currentTime = Math.max(0, video.currentTime - frameRate);
@@ -10,4 +8,28 @@ const frameBack = () => {
 
 const frameForward = () => {
     video.currentTime = Math.min(video.duration, video.currentTime + frameRate);
+}
+
+//Set Start and Stop values
+
+let startTime;
+let stopTime;
+
+const setStart = () => {
+    startTime = video.currentTime;
+    console.log(startTime);
+}
+
+const setStop = () => {
+    stopTime = video.currentTime;
+    console.log(stopTime);
+}
+
+//Calculate time between start and stop
+let time;
+const timeDisplay = document.querySelector('.timeDisplay');
+
+const calcTime = () => {
+    time = stopTime - startTime;
+    timeDisplay.textContent = time.toFixed(3)
 }
